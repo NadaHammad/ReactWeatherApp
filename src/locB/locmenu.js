60519de5acb3bba5fd071a12af455286dfab049d
Maki <annelynmccormick@yahoo.com>
2021-03-19 15:36:23 +0000

    css for the location bttns
    Refined the css for the menu display of location and its buttons

--- src/locB/locmenu.js
+++ src/locB/locmenu.js
@@ -1,40 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import  './locbttn.css';
 
-export default class LocMenu extends React.Component{
+export default function LocMenu(props){
 
-    // Two states to handle for now: Location of choice and temperature of said location of choice
-    state = {
-        area: 'AreaA',
-        display: false
-    }
-
-    render() {
-        //console.log('this si the menu saying yo yo yo!');
-
+    const [area, setArea] = useState("tempA");
+    const [display, setD] = useState("tempD");
 
     return (
         // Renders a banner via 'DIV' and using 'p'
             <div className="MenuBox">
-                <h3>Choose Locations to Edit:</h3>
+                <h4>Choose Locations to Edit:</h4>
                     <section>
                         <p className="titleP">Location1:</p>
                             <section className="areaP">
-                                <p>{this.state.area}</p>
+                                <p>{area}</p>
                                 <section>
-                                    <button className="editB">edit</button>
-                                    <button className="delB">delete</button>
+                                    <button type="submit" className="editB">edit</button>
+                                    <button type="submit" className="delB">delete</button>
                                 </section>
                             </section>
                     </section>
 
                     <section>
                         <p className="titleP"> Location2:</p>
-                            <section className="reaP">
-                                <p>{this.state.area}</p>
+                            <section className="areaP">
+                                <p>{area}</p>
                                 <section>
-                                    <button className="editB">edit</button>
-                                    <button className="delB">delete</button>
+                                    <button type="submit" className="editB">edit</button>
+                                    <button type="submit" className="delB">delete</button>
                                 </section>
                             </section>
                     </section>
@@ -42,18 +35,20 @@ export default class LocMenu extends React.Component{
                     <section>
                         <p className="titleP">Location3:</p>
                             <div className="areaP">
-                                <p>{this.state.area}</p>
+                                <p>{area}</p>
                                 <section>
-                                    <button className="editB">edit</button>
-                                    <button className="delB">delete</button>
+                                    <button type="submit" className="editB">edit</button>
+                                    <button type="submit" className="delB">delete</button>
                                 </section>
                             </div>
                     </section>
 
-                <button type="submit">Confirm</button>
-                <button type="submit">Cancel</button>
+                    <div className="concanMain">
+                        <section className="concan">
+                            <button className="con" type="submit">Confirm</button>
+                            <button className="can" type="submit">Cancel</button>
+                        </section>
+                    </div>
             </div>
         );
-    }
-    
 }
--- src/locB/mnglocbttn.js
+++ src/locB/mnglocbttn.js
@@ -4,20 +4,20 @@ import LocMenu from './locmenu';
 
 export default function ManageLoc(props) {
 
-
+    const [displayMenu, setD] = useState("true");
+    
     return (
         // Renders a banner via 'DIV' and using 'p'
         <div className="mngDIV">
             <div className="mngbttn">
-                <button type="submit" >
+                <button type="submit" onClick={() => setD(!displayMenu)}>
                     <p className="locP">Manage Locations</p>
                 </button>
             </div>
 
             <div>
-                {/* {displayMenu.display ?  <LocMenu/> : null } */}
+                {displayMenu ?  null : <LocMenu/> }
             </div>
         </div>
         );
-    
 }
