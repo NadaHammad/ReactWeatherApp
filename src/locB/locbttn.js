a2378dcd1a23b0a00d540bf866be856ec1dde785
Maki <annelynmccormick@yahoo.com>
2021-03-27 17:32:27 +0000

    commented locbttn.js

--- src/locB/locbttn.js
+++ src/locB/locbttn.js
@@ -1,10 +1,12 @@
 import "./locbttn.css"; // import required Components from 'components/'
 import React, { useContext, useState } from 'react';
-import {LocationList} from "../components/iphone/index";
+import {LocationList} from "../components/iphone/index"; // IMPORT THE CONTEXT
 
+// THE LOCATION BANNER
 export default function Locbttn(props) {
-    const [locationData, setLocationData]  =  React.useContext(LocationList);
+    const [locationData, setLocationData]  =  React.useContext(LocationList); // PASS THE CONTEXT, which are the 3 locations
 
+    //  The data such as name and temperature fo the locations are then rendered here, by calling the constant that has the context
     return (
         <div  className="locbttn" >
             <div className="locbanner">
